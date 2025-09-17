42d5674ea74f84b4a44c55906ccbd397db4f6b39
Martins <olatunosunmartins14@gmail.com>
2025-09-17 06:32:08 +0100

    uf

--- Line.java
+++ Line.java
@@ -3,8 +3,8 @@
 - Assign user input to variables x1 and y1 respectively
 - Prompt user to enter a point(x2 and y2)
 - Assign user input to variables x2 and y2 respectively
-- Check if x1 is equal to x2. If so print "this line is perpendicular to the x axis". If not
-- Check if y1 is equal to y2. If so print "This line is perpendicular to the y axis". If not
+- Check if x1 is equal to x2. If so print "this line is perpendicular to the y axis". If not
+- Check if y1 is equal to y2. If so print "This line is perpendicular to the x axis". If not
 - Print "The line isn't perpendicular to any axis"
 */
 
@@ -23,10 +23,10 @@ public static void main(String [] args){
 	int y2 = input.nextInt();
 
 	if (x1 == x2){
-		System.out.println("The line is perpendicular to the x axis.");
+		System.out.println("The line is perpendicular to the y axis.");
 }
 	else if (y1 == y2){
-		System.out.println("The line is perpendicular to the y axis.");
+		System.out.println("The line is perpendicular to the x axis.");
 }
 	else{
 		System.out.println("The line is not perpendicular to any axis."); 
